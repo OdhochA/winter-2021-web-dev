d61bbc4185a7e24f3faf0750e976468e3c950d13
Odhochakinyi <vivianrose719@gmail.com>
2021-12-01 00:59:33 +0300

    Repository Update

---
+++ main.js
@@ -0,0 +1,36 @@
+// Image switcher code
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/firefox-icon.png') {
+    myImage.setAttribute ('src','Images/firefox-icon.png');
+  } else {
+    myImage.setAttribute ('src','Images/firefox-icon.png');
+  }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'I am cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'I am cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
